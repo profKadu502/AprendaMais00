13831a9116fdd17731bdc56ca379bbda88b90c73
profKadu502 <profkadu502@gmail.com>
2026-04-21 15:44:05 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. ARQUIVO: script.js
+
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
